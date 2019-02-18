0747397ab2f7a9c63d6cc7377c74cf80e4298ea7
Chris Withers <chris@withers.org>
2019-02-18 06:38:41 +0000

    try triggering a travis build

--- travis.py
+++ travis.py
@@ -7,3 +7,19 @@ travis.headers['Travis-API-Version'] = '3'
 travis.headers['Authorization'] = 'token '+os.environ.get('TRAVIS_TOKEN')
 response = travis.get('https://api.travis-ci.org/repo/cjw296%2Fplayground')
 pprint(response.json())
+
+if os.environ.get('DO_RELEASE'):
+    print('Doing release!')
+else:
+    print('Triggering release')
+    response = travis.post('https://api.travis-ci.org/repo/cjw296%2Fplayground/requests', json={
+      "request": {
+        "message": "Releasing 0.1.2",
+        "branch": "master",
+        "config": {
+          "env": {
+            "DO_RELEASE": "yes"
+          },
+        }
+      }
+    })
